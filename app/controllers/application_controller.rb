4d5b0e632888c31634787266ff6bca1cc96dc034
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-23 13:26:36 +0000

    6-7 devise_2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+        # 全てのコントローラに対する処理を行える権限を持つ
+       
+  before_action :configure_permitted_parameters, if: :devise_controller?
+        # 
+  protected
+  
+  def configure_permitted_parameters
+        # メソッド
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        # メソッド ユーザー登録(sign_up)の際に、
+        # ユーザー名(name)のデータ操作を許可
+        # ストリングパラメータと同様の機能
+  end
+  
+  
 end
