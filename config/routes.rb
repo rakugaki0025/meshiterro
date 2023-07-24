3d46466703fbcafd2d08661a99cc0396b5c6cf24
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-24 12:46:43 +0000

    about finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+  
   root to: "homes#top"
-
+  get 'homes/about' => "homes#about"
+    # 取得 'アドレス' => 指定のコントローラー#アクション
+  
   # resources :woods
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
